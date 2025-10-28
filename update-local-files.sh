20f4f6c3e27fe7affb08ae0a39dba1af16f10d3a
Denis Rylikov <63297132+drylikov@users.noreply.github.com>
2025-10-28 15:11:31 +0300

    Update update-local-files.sh

--- update-local-files.sh
+++ update-local-files.sh
@@ -4,6 +4,7 @@
 #!/bin/bash
 
 
+
 rm -rfv /home/dorian/.mozilla/firefox/8ned6ue0.default/gm_scripts/Github_Contributions_Heatmap/*
 
 ln -sv /media/data/www/github-contributions-heatmap/github-contributions-heatmap.user.js /home/dorian/.mozilla/firefox/8ned6ue0.default/gm_scripts/Github_Contributions_Heatmap/
